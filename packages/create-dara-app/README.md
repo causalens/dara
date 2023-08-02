4524a653baad221d41c98e28aa75f7e951cf6a48
Krzysztof Bielikowicz <87647189+krzysztof-causalens@users.noreply.github.com>
2023-08-02 12:26:48 +0100

    Build:1.0.0 (#21)
    * Version 1.0.0
    
    * Update UI deps to 1.0.0
    
    * Testing: absolute links to GH hosted files instead of relative in README
    
    * Testing: absolute svg links
    
    * Use top-level README for dara-core
    
    * Testing: symlink top-level README instead
    
    * WIP: script to update absolute readme links to the tag version
    
    * Use a Python script to fix READMEs for PyPi. Add READMEs for other packages
    
    * TEST: readme update script
    
    * Wrong script path
    
    * Testing succeeded, cleanup
    
    * Add npm version badges

--- packages/create-dara-app/README.md
+++ packages/create-dara-app/README.md
@@ -1,5 +1,31 @@
-***************************
-CREATE-DARA-APP
-***************************
+# Create Dara App
 
+<picture>
+    <source srcset="https://github.com/causalens/dara/blob/master/img/dara_dark.svg?raw=true" media="(prefers-color-scheme: dark)">
+    <img src="https://github.com/causalens/dara/blob/master/img/dara_light.svg?raw=true" alt="Dara Logo">
+</picture>
+
+![Master tests](https://github.com/causalens/dara/actions/workflows/tests.yml/badge.svg?branch=master)
 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)
+[![PyPI](https://img.shields.io/pypi/v/create-dara-app.svg?color=dark-green)](https://pypi.org/project/create-dara-app/)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/create-dara-app.svg?color=dark-green)](https://pypi.org/project/create-dara-app/)
+
+### Build decision apps in Python
+
+_Tap into the power of causality by transforming data into interactive graphs and applications_
+
+Dara is a dynamic application framework designed for creating interactive web apps with ease, all in pure Python. Over the past two years, Dara has fueled the development of hundreds of apps, now widely used and appreciated by both our customers and our in-house teams!
+
+## Quickstart
+
+To install the package, run:
+
+```bash
+pip install create-dara-app
+```
+
+That's it! You can now use the `create-dara-app` CLI tool to create a new app.
+
+```bash
+create-dara-app
+```
---
+++ tooling/scripts/update-readmes.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import re
+
+def replace_picture_with_img(match):
+    # Grab the content inside <picture>...</picture>
+    picture_content = match.group(0)
+
+    # Find the img src inside the <picture> content
+    img_match = re.search(r'<img src="([^"]*)"', picture_content)
+    if img_match:
+        return f'<img src="{img_match.group(1)}">'
+    else:
+        return ""
+
+# Check if a version argument is provided
+if len(sys.argv) < 2:
+    print("Usage: ./update-readmes.py <version>")
+    print("Example: ./update-readmes.py v0.1.0")
+    sys.exit(1)
+
+# Loop through each packages/x folder
+for dir_path, dirs, files in os.walk("packages/"):
+    # Check if README.md file exists
+    if "README.md" in files:
+        readme_file = os.path.join(dir_path, "README.md")
+
+        with open(readme_file, 'r+') as file:
+            content = file.read()
+
+            # 1. Replace /dara/blob/master/ with /dara/blob/<version>/.
+            # This is to ensure that the README.md file always points to the correct tag on GitHub
+            new_content = re.sub(r'/dara/blob/master/', f'/dara/blob/{sys.argv[1]}/', content)
+
+            # 2. Replace <picture> tag with <img> tag for PyPI
+            # This is because PyPI doesn't support <picture> tags yet
+            # Assuming the <picture> tag looks something like this:
+            # <picture>
+            #   ...
+            #   <img src="default">
+            # </picture>
+            # We want to replace it with:
+            # <img src="default">
+            new_content = re.sub(r'<picture>.*?</picture>', replace_picture_with_img, new_content, flags=re.DOTALL)
+
+            # If content is changed, write it back to the file
+            if new_content != content:
+                file.seek(0)
+                file.write(new_content)
+                file.truncate()
+
+print("Update complete.")
