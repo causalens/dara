985df8725b784f36252a2954b35949ac1e35c838
Patricia Jacob <104359539+patricia-causalens@users.noreply.github.com>
2023-09-28 20:04:06 +0100

    exposing import needed for internal version of graph (#83)

--- packages/dara-components/js/graphs/index.tsx
+++ packages/dara-components/js/graphs/index.tsx
@@ -1,3 +1,9 @@
+import type { GraphLayoutDefinition } from './graph-layout';
+
 export { default as CausalGraphViewer } from './causal-graph-viewer';
 export { default as NodeHierarchyBuilder } from './node-hierarchy-builder';
 export { default as VisualEdgeEncoder } from './visual-edge-encoder';
+export { parseLayoutDefinition } from './graph-layout';
+
+// Types are exported separately because esbuild (used by Vite) doesn't support type-only re-exports directly
+export type { GraphLayoutDefinition };
