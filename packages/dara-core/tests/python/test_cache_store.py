d5e33383cf2acd7dac6f3b6d34d0c1561b33dd73
Krzysztof Bielikowicz <krzysztof@causalens.com>
2023-09-29 17:58:05 +0100

    Add extra unit tests for public api of cache store

--- packages/dara-core/tests/python/test_cache_store.py
+++ packages/dara-core/tests/python/test_cache_store.py
@@ -1,9 +1,11 @@
 import pytest
 from freezegun import freeze_time
 
+from dara.core.auth.definitions import SESSION_ID, USER, UserData
+from dara.core.internal.cache_store.cache_store import CacheStore
 from dara.core.internal.cache_store.ttl import TTLCache
 from dara.core.internal.cache_store.lru import LRUCache
-from dara.core.base_definitions import Cache
+from dara.core.base_definitions import Cache, CachedRegistryEntry
 
 pytestmark = pytest.mark.anyio
 
@@ -162,3 +164,111 @@ async def test_ttl_cache():
         assert await ttl_cache.get("d") is None  # "d" should be evicted since it's no longer pinned
         assert await ttl_cache.get("e") is None  # "e" should be evicted since it's no longer pinned
         assert await ttl_cache.get("f") is None  # "f" should be evicted since it's no longer pinned
+
+async def test_cache_store_global_api():
+    # Sample store, we're not testing cache eviction so just use keep-all here
+    store = CacheStore()
+    reg_entry = CachedRegistryEntry(uid='test_uid', cache=Cache.Policy.KeepAll())
+
+    # Simple add and get
+    await store.set(reg_entry, key='test_key', value='test_value')
+    assert await store.get(reg_entry, key='test_key') == 'test_value'
+
+    # Empty store
+    await store.clear()
+    assert await store.get(reg_entry, key='test_key') is None
+
+async def test_cache_store_session_api():
+    # Sample store, we're not testing cache eviction so just use keep-all here
+    store = CacheStore()
+    reg_entry = CachedRegistryEntry(uid='test_uid', cache=Cache.Policy.KeepAll(cache_type=Cache.Type.SESSION))
+
+    # Test session values are separated
+    SESSION_ID.set('session_1')
+    await store.set(reg_entry, key='test_key', value='test_value')
+
+    SESSION_ID.set('session_2')
+    assert await store.get(reg_entry, key='test_key') is None
+    await store.set(reg_entry, key='test_key', value='test_value_2')
+
+    SESSION_ID.set('session_1')
+    assert await store.get(reg_entry, key='test_key') == 'test_value'
+
+    SESSION_ID.set('session_2')
+    assert await store.get(reg_entry, key='test_key') == 'test_value_2'
+
+async def test_cache_store_user_api():
+    # Sample store, we're not testing cache eviction so just use keep-all here
+    store = CacheStore()
+    reg_entry = CachedRegistryEntry(uid='test_uid', cache=Cache.Policy.KeepAll(cache_type=Cache.Type.USER))
+
+    # Test user values are separated
+    USER.set(
+        UserData(
+            identity_name='test1',
+        )
+    )
+    await store.set(reg_entry, key='test_key', value='test_value')
+
+    USER.set(
+        UserData(
+            identity_name='test2',
+        )
+    )
+    assert await store.get(reg_entry, key='test_key') is None
+    await store.set(reg_entry, key='test_key', value='test_value_2')
+
+    USER.set(
+        UserData(
+            identity_name='test1',
+        )
+    )
+    assert await store.get(reg_entry, key='test_key') == 'test_value'
+
+    USER.set(
+        UserData(
+            identity_name='test2',
+        )
+    )
+    assert await store.get(reg_entry, key='test_key') == 'test_value_2'
+
+async def test_cache_store_wait_and_get():
+    store = CacheStore()
+    reg_entry = CachedRegistryEntry(uid='test_uid', cache=Cache.Policy.KeepAll())
+
+    await store.set_pending(reg_entry, key='test_key')
+
+    # Create two coroutines that are trying to access the result
+    var_1 = store.get_or_wait(reg_entry, key='test_key')
+    var_2 = store.get_or_wait(reg_entry, key='test_key')
+
+    # Set the value
+    await store.set(reg_entry, key='test_key', value='test_value')
+
+    # Check the values are resolved
+    assert await var_1 == 'test_value'
+    assert await var_2 == 'test_value'
+
+async def test_cache_store_pinning():
+    store = CacheStore()
+    reg_entry = CachedRegistryEntry(uid='test_uid', cache=Cache.Policy.MostRecent())
+
+    # Add a pinned entry
+    await store.set(reg_entry, key='test_key', value='test_value', pin=True)
+
+    # Add a non-pinned entry
+    await store.set(reg_entry, key='test_key_2', value='test_value_2')
+    # Pinned entry was preserved, non-pinned violated the eviction rule so is None
+    assert await store.get(reg_entry, key='test_key') == 'test_value'
+    assert await store.get(reg_entry, key='test_key_2') is None
+
+    # Unpin the pinned entry
+    assert await store.get(reg_entry, key='test_key', unpin=True) == 'test_value'
+
+    # Add a new entry
+    await store.set(reg_entry, key='test_key_3', value='test_value_3')
+
+    # Check the original entry was evicted
+    assert await store.get(reg_entry, key='test_key') is None
+    assert await store.get(reg_entry, key='test_key_2') is None
+    assert await store.get(reg_entry, key='test_key_3') == 'test_value_3'
