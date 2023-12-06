45eda7d6744c1c91f3f070379bf9f28a49bd3bcb
Patricia Jacob <patricia@causalens.com>
2023-12-06 12:02:24 +0000

    added clear local

--- packages/dara-core/js/shared/utils/clean-session-cache.tsx
+++ packages/dara-core/js/shared/utils/clean-session-cache.tsx
@@ -6,11 +6,19 @@
  */
 function cleanSessionCache(sessionToken: string): void {
     const sessionKeys = Object.keys(sessionStorage);
+    const localKeys = Object.keys(localStorage);
 
     sessionKeys.forEach((key) => {
         // Remove keys related to a different Dara session
         if (key.startsWith('dara-session') && !key.startsWith(`dara-session-${sessionToken}`)) {
             sessionStorage.removeItem(key);
+        }
+    });
+
+    localKeys.forEach((key) => {
+        // Remove keys related to a different Dara session such as persisted Variables
+        if (key.startsWith('dara-session') && !key.startsWith(`dara-session-${sessionToken}`)) {
+            localStorage.removeItem(key);
         }
     });
 }
