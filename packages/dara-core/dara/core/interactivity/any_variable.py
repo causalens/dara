8467349a436fc54d5002fcb1c0b6a1a08fa8639e
sam-causalens <58470795+sam-causalens@users.noreply.github.com>
2024-01-18 13:59:04 +0000

    Fix: DOS-577 Address failure of get_current_value (#176)
    * Address the websocket session not being configured correctly in get_current_value after navigating away and back to a running kernel
    * The double if block ended up with a situation where the sessions could be empty even though a session existed which lead to the request failing even though there was a connection to use.
    * Add a test covering the tweak to the logic

--- packages/dara-core/dara/core/interactivity/any_variable.py
+++ packages/dara-core/dara/core/interactivity/any_variable.py
@@ -142,9 +142,8 @@ async def get_current_value(variable: dict, timeout: float = 3, raw: bool = Fals
             if websocket_registry.has(sid):
                 ws_channels = websocket_registry.get(sid)
                 # If saved websocket channel is not none, then only save the session of that channel
-                if saved_ws_channel is not None:
-                    if saved_ws_channel in ws_channels:
-                        session_channels[sid] = {saved_ws_channel}
+                if saved_ws_channel is not None and saved_ws_channel in ws_channels:
+                    session_channels[sid] = {saved_ws_channel}
                 else:
                     session_channels[sid] = ws_channels
 
