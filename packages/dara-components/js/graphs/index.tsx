5723e5d238a0ef50887c7f73a46ee8d242471dd9
Patricia Jacob <patricia@causalens.com>
2024-02-14 16:30:26 +0000

    added missing export

--- packages/dara-components/js/graphs/index.tsx
+++ packages/dara-components/js/graphs/index.tsx
@@ -4,6 +4,7 @@ export { default as CausalGraphViewer } from './causal-graph-viewer';
 export { default as NodeHierarchyBuilder } from './node-hierarchy-builder';
 export { default as VisualEdgeEncoder } from './visual-edge-encoder';
 export { parseLayoutDefinition } from './graph-layout';
+export { transformLegendColor } from './utils';
 
 // Types are exported separately because esbuild (used by Vite) doesn't support type-only re-exports directly
 export type { GraphLayoutDefinition };
