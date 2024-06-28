8b80942ff190b527e3b7b3c7bce148fac71f2fdc
Krzysztof Bielikowicz <krzysztof@causalens.com>
2024-06-28 11:38:01 +0100

    Refactor to make toggle work

--- packages/dara-core/js/actions/update-variable.tsx
+++ packages/dara-core/js/actions/update-variable.tsx
@@ -15,7 +15,7 @@ export const TOGGLE = '__dara_toggle__';
 /**
  * Front-end handler for UpdateVariable action.
  */
-const UpdateVariable: ActionHandler<UpdateVariableImpl> = (ctx, actionImpl) => {
+const UpdateVariable: ActionHandler<UpdateVariableImpl> = async (ctx, actionImpl) => {
     let varAtom;
     let eventName: 'PLAIN_VARIABLE_LOADED' | 'URL_VARIABLE_LOADED';
 
@@ -33,22 +33,21 @@ const UpdateVariable: ActionHandler<UpdateVariableImpl> = (ctx, actionImpl) => {
             throw new Error('DataVariable is not supported in UpdateVariable action');
     }
 
+    let newValue;
+
     if (actionImpl.value === INPUT) {
-        ctx.set(varAtom, ctx.input);
-        ctx.eventBus.publish(eventName, { variable: actionImpl.variable as any, value: ctx.input });
-        return;
+        newValue = ctx.input;
+    } else if (actionImpl.value === TOGGLE) {
+        // normally we'd use the updater form here, but we need to know what value we're
+        // toggling to emit the correct event, and the updater must be pure
+        const value = await ctx.snapshot.getLoadable(varAtom).toPromise();
+        newValue = !value;
+    } else {
+        newValue = actionImpl.value;
     }
 
-    if (actionImpl.value === TOGGLE) {
-        ctx.set(varAtom, (value: boolean) => {
-            ctx.eventBus.publish(eventName, { variable: actionImpl.variable as any, value: !value });
-            return !value;
-        });
-        return;
-    }
-
-    ctx.set(varAtom, actionImpl.value);
-    ctx.eventBus.publish(eventName, { variable: actionImpl.variable as any, value: actionImpl.value });
+    ctx.set(varAtom, newValue);
+    ctx.eventBus.publish(eventName, { variable: actionImpl.variable as any, value: newValue });
 };
 
 export default UpdateVariable;
