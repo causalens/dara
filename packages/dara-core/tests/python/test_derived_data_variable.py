2e094a014bb2625c8ba839ba53356a4b770c0a8c
Roman Kornev <roman@causalens.com>
2024-08-27 15:02:00 +0100

    typo

--- packages/dara-core/tests/python/test_derived_data_variable.py
+++ packages/dara-core/tests/python/test_derived_data_variable.py
@@ -298,12 +298,12 @@ async def test_derived_data_variable_no_filters():
         assert mock_func.call_count == 2   # not called again
 
         # Check schema can be returned correctly
-        scshema_response = await client.get(
+        schema_response = await client.get(
             '/api/core/data-variable/uid/schema?cache_key=' + third_response.json()['cache_key'],
             headers=AUTH_HEADERS,
         )
-        assert scshema_response.status_code == 200, scshema_response.text
-        schema = scshema_response.json()
+        assert schema_response.status_code == 200, schema_response.text
+        schema = schema_response.json()
         assert {'name': '__col__1__col1', 'type': 'integer'} in schema['fields']
         assert {'name': '__col__2__col2', 'type': 'integer'} in schema['fields']
         assert {'name': '__col__3__col3', 'type': 'string'} in schema['fields']
