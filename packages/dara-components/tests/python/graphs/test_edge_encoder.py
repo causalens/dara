e5a04160d58928a8c97a8d20d84bedfd0a0da5c5
Krzysztof Bielikowicz <krzysztof@causalens.com>
2025-04-14 10:02:46 +0100

    Add test

---
+++ packages/dara-components/tests/python/graphs/test_edge_encoder.py
@@ -0,0 +1,58 @@
+from cai_causal_graph import CausalGraph
+from cai_causal_graph.graph_components import Node
+from fastapi.encoders import jsonable_encoder
+
+from dara.components.graphs import VisualEdgeEncoder
+from dara.core import Variable
+
+
+def test_serialize():
+    # as strings
+    encoder = VisualEdgeEncoder(nodes=['Age', 'Unemployment', 'Education', 'Income'])
+    encoded = jsonable_encoder(encoder)
+
+    assert encoded['props']['nodes'] == ['Age', 'Unemployment', 'Education', 'Income']
+
+    # as dict of [str, Node]
+    encoder = VisualEdgeEncoder(
+        nodes={
+            'Age': Node('Age'),
+            'Unemployment': Node('Unemployment'),
+            'Education': Node('Education'),
+            'Income': Node('Income'),
+        }
+    )
+    encoded = jsonable_encoder(encoder)
+
+    assert encoded['props']['nodes'] == {
+        'Age': encoder.nodes['Age'].to_dict(),
+        'Unemployment': encoder.nodes['Unemployment'].to_dict(),
+        'Education': encoder.nodes['Education'].to_dict(),
+        'Income': encoder.nodes['Income'].to_dict(),
+    }
+
+    # as variable with list of strings
+    encoder = VisualEdgeEncoder(nodes=Variable(default=['Age', 'Unemployment', 'Education', 'Income']))
+    encoded = jsonable_encoder(encoder)
+
+    assert encoded['props']['nodes']['default'] == ['Age', 'Unemployment', 'Education', 'Income']
+
+    # as variable with dict of [str, Node]
+    encoder = VisualEdgeEncoder(
+        nodes=Variable(
+            default={
+                'Age': Node('Age'),
+                'Unemployment': Node('Unemployment'),
+                'Education': Node('Education'),
+                'Income': Node('Income'),
+            }
+        )
+    )
+    encoded = jsonable_encoder(encoder)
+
+    assert encoded['props']['nodes']['default'] == {
+        'Age': encoder.nodes.default['Age'].to_dict(),
+        'Unemployment': encoder.nodes.default['Unemployment'].to_dict(),
+        'Education': encoder.nodes.default['Education'].to_dict(),
+        'Income': encoder.nodes.default['Income'].to_dict(),
+    }
