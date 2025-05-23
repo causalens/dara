797a8ef652adc6dbcdfa866f7917c68f8e1d780d
Krzysztof Bielikowicz <krzysztof@causalens.com>
2025-05-23 14:21:55 +0100

    WIP: subscribe to backend store method

--- packages/dara-core/dara/core/persistence.py
+++ packages/dara-core/dara/core/persistence.py
@@ -1,7 +1,7 @@
 import abc
 import json
 import os
-from typing import TYPE_CHECKING, Any, Dict, Literal, Optional, Set
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, Dict, Literal, Optional, Set
 from uuid import uuid4
 
 import aiorwlock
@@ -21,13 +21,12 @@ from dara.core.internal.websocket import WS_CHANNEL
 
 if TYPE_CHECKING:
     from dara.core.interactivity.plain_variable import Variable
+    from dara.core.internal.websocket import WebsocketManager
 
 
 class PersistenceBackend(BaseModel, abc.ABC):
     """
-    Abstract base class for a BackendStore backend
-
-    Warning: the API is not stable yet and may change in the future
+    Abstract base class for a BackendStore backend.
     """
 
     @abc.abstractmethod
@@ -59,6 +58,13 @@ class PersistenceBackend(BaseModel, abc.ABC):
         """
         Get all the values as a dictionary of key-value pairs
         """
+
+    async def subscribe(self, on_value: Callable[[str, Any], Awaitable[None]]):
+        """
+        Subscribe to changes in the backend. Called with a callback that should be invoked whenever a value is updated.
+        """
+        # Default implementation does nothing, not all backends need to support this
+        pass
 
 
 class InMemoryBackend(PersistenceBackend):
@@ -235,6 +241,17 @@ class BackendStore(PersistenceStore):
 
         raise ValueError('User not found when trying to compute the key for a user-scoped store')
 
+    def _get_user(self, key: str) -> str | None:
+        """
+        Get the user for a given key. Returns None if the key is global.
+        Reverts the `_get_key` method to get the user for a given key.
+        """
+        if key == 'global':
+            return None
+
+        # otherwise key is a user identity_id or identity_name
+        return key
+
     def _register(self):
         """
         Register this store in the backend store registry.
@@ -253,29 +270,51 @@ class BackendStore(PersistenceStore):
         except ValueError as e:
             raise ValueError('BackendStore uid must be unique') from e
 
-    async def _notify(self, value: Any):
+    @property
+    def ws_mgr(self) -> WebsocketManager:
+        from dara.core.internal.registries import utils_registry
+
+        return utils_registry.get('WebsocketManager')
+
+    def _create_msg(self, value: Any) -> Dict[str, Any]:
         """
-        Notify all clients about the new value for this store
+        Create a message to send to the frontend.
+        :param value: value to send
+        """
+        return {'store_uid': self.uid, 'value': value}
+
+    async def _notify_user(self, user_identifier: str, value: Any):
+        """
+        Notify a given user about the new value for this store.
+        :param user_identifier: user to notify
+        :param value: value to notify about
+        """
+        return await self.ws_mgr.send_message_to_user(
+            user_identifier,
+            self._create_msg(value),
+            ignore_channel=WS_CHANNEL.get(),
+        )
+
+    async def _notify_global(self, value: Any):
+        """
+        Notify all users about the new value for this store.
+        :param value: value to notify about
+        """
+        return await self.ws_mgr.broadcast(
+            self._create_msg(value),
+            ignore_channel=WS_CHANNEL.get(),
+        )
+
+    async def _notify_value(self, value: Any):
+        """
+        Notify all clients about the new value for this store.
+        Broadcasts to all users if scope is global or sends to the current user if scope is user.
 
         :param value: value to notify about
         """
-        from dara.core.internal.registries import utils_registry
-        from dara.core.internal.websocket import WebsocketManager
-
-        ws_mgr: WebsocketManager = utils_registry.get('WebsocketManager')
-        msg = {'store_uid': self.uid, 'value': value}
-
         if self.scope == 'global':
-            return await ws_mgr.broadcast(msg, ignore_channel=WS_CHANNEL.get())
-
-        # For user scope, we need to find channels for the user and notify them
-        user = USER.get()
-
-        if not user:
-            return
-
-        user_identifier = user.identity_id or user.identity_name
-        return await ws_mgr.send_message_to_user(user_identifier, msg, ignore_channel=WS_CHANNEL.get())
+            return await self._notify_global(value)
+        return await self._notify_user(self.scope, value)
 
     async def init(self, variable: 'Variable'):
         """
@@ -285,6 +324,13 @@ class BackendStore(PersistenceStore):
         """
         self._register()
         self.default_value = variable.default
+
+        async def _on_value(key: str, value: Any):
+            if user := self._get_user(key):
+                return await self._notify_user(user, value)
+            return await self._notify_global(value)
+
+        await self.backend.subscribe(_on_value)
 
     async def write(self, value: Any, notify=True):
         """
@@ -299,7 +345,7 @@ class BackendStore(PersistenceStore):
         key = await self._get_key()
 
         if notify:
-            await self._notify(value)
+            await self._notify_value(value)
 
         return await run_user_handler(self.backend.write, (key, value))
 
@@ -328,7 +374,7 @@ class BackendStore(PersistenceStore):
         key = await self._get_key()
         if notify:
             # Schedule notification on delete
-            await self._notify(None)
+            await self._notify_value(None)
         return await run_user_handler(self.backend.delete, (key,))
 
     async def get_all(self) -> Dict[str, Any]:
