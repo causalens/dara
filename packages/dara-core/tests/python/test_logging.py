76e7c829e35e94653f4da40ce38e2f870d643ab9
ilya-causalens <ilya@causalens.com>
2025-06-02 13:08:15 +0100

    fixed test

--- packages/dara-core/tests/python/test_logging.py
+++ packages/dara-core/tests/python/test_logging.py
@@ -2,37 +2,28 @@ import logging
 from dara.core.logging import DaraDevFormatter
 from colorama import Back, Fore
 
+
 def test_custom_level():
     HIGHER_INFO = int((logging.INFO + logging.WARNING) / 2)
-    logging.addLevelName(HIGHER_INFO, "HIGHER_INFO")
+    logging.addLevelName(HIGHER_INFO, 'HIGHER_INFO')
     HIGHER_WARNING = int((logging.WARNING + logging.ERROR) / 2)
-    logging.addLevelName(HIGHER_WARNING, "HIGHER_WARNING")
+    logging.addLevelName(HIGHER_WARNING, 'HIGHER_WARNING')
 
     formatter = DaraDevFormatter()
 
-    logger = logging.getLogger("test_logger")
+    logger = logging.getLogger('test_logger')
     fn, lno, func, sinfo = logger.findCaller()
     args, exc_info, extra = (), None, None
-    msg = "Sample message"
+    msg = 'Sample message'
 
-    record = logger.makeRecord(logger.name, HIGHER_INFO, fn, lno, msg, args,
-                                 exc_info, func, extra, sinfo)
+    record = logger.makeRecord(logger.name, HIGHER_INFO, fn, lno, msg, args, exc_info, func, extra, sinfo)
     colors = formatter._resolve_record_color(record)
     assert colors == (Back.GREEN, Fore.GREEN)
 
-    record = logger.makeRecord(logger.name, HIGHER_INFO, fn, lno, msg, args,
-                                 exc_info, func, extra, sinfo)
+    record = logger.makeRecord(logger.name, HIGHER_WARNING, fn, lno, msg, args, exc_info, func, extra, sinfo)
     colors = formatter._resolve_record_color(record)
     assert colors == (Back.YELLOW, Fore.YELLOW)
 
-
-    record = logger.makeRecord(logger.name, HIGHER_INFO, fn, lno, msg, args,
-                                 exc_info, func, extra, sinfo)
+    record = logger.makeRecord(logger.name, logging.DEBUG, fn, lno, msg, args, exc_info, func, extra, sinfo)
     colors = formatter._resolve_record_color(record)
-    assert colors == (Back.YELLOW, Fore.YELLOW)
-
-
-
-
-
-    
+    assert colors == (Back.WHITE, Fore.RESET)
