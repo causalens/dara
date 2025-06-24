7de34c4dd43f3c49f10a43455963987b7695fdc7
Krzysztof Bielikowicz <krzysztof@causalens.com>
2025-06-24 11:10:15 +0100

    Docstring typo
    Co-authored-by: Roman-Kornev <119343173+Roman-Kornev@users.noreply.github.com>

--- packages/dara-core/js/shared/interactivity/condition-or-variable.tsx
+++ packages/dara-core/js/shared/interactivity/condition-or-variable.tsx
@@ -7,7 +7,7 @@ import { useVariable } from './use-variable';
 
 /* eslint-disable react-hooks/rules-of-hooks */
 export function useConditionOrVariable<T>(arg: Variable<T> | Condition<T> | T): T {
-    // Note we assume arge never changes from a condition to a variable so we can use hooks conditionally
+    // Note we assume arg never changes from a condition to a variable so we can use hooks conditionally
     if (isCondition(arg)) {
         const value = useVariable(arg.variable)[0];
         const other = useVariable(arg.other)[0];
