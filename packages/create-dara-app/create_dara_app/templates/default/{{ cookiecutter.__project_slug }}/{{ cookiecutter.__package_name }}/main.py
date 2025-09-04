0c08c585ff32feec85a1dc9852d0b39236adc1b4
Krzysztof Bielikowicz <krzysztof@causalens.com>
2025-09-04 09:30:13 +0100

    Update create-dara-app template to use new api

--- packages/create-dara-app/create_dara_app/templates/default/{{ cookiecutter.__project_slug }}/{{ cookiecutter.__package_name }}/main.py
+++ packages/create-dara-app/create_dara_app/templates/default/{{ cookiecutter.__project_slug }}/{{ cookiecutter.__package_name }}/main.py
@@ -1,17 +1,34 @@
 from {{cookiecutter.__package_name}}.pages.components_page import components_page
 from {{cookiecutter.__package_name}}.pages.intro_page import intro_page
-from {{cookiecutter.__package_name}}.utils.template import template_renderer
 
-from dara.core import ConfigurationBuilder
+from dara.core import ConfigurationBuilder, MenuLink, Outlet, SideBarFrame
 from dara.core.css import get_icon
+
+from dara.components import Icon, Stack, Text
 
 # Create the configuration builder
 config = ConfigurationBuilder()
 
-# Add the template renderer to the configuration
-config.add_template_renderer('side-bar', template_renderer)
-config.template = 'side-bar'
+# Root layout that displays a sidebar with links to the two pages
+def RootLayout():
+    return SideBarFrame(
+        content=Outlet(),
+        side_bar=Stack(
+            MenuLink(
+                Icon(icon=get_icon('newspaper')),
+                Text('Welcome'),
+                to='/',
+            ),
+            MenuLink(
+                Icon(icon=get_icon('spell-check')),
+                Text('A-Z Components'),
+                to='/components',
+            ),
+        )
+    )
 
-# Add the pages to the configuration
-config.add_page(name='Welcome', content=intro_page(), icon=get_icon('newspaper'))
-config.add_page(name='A-Z Components', content=components_page(), icon=get_icon('spell-check'))
+# Add the layout and pages to the configuration
+root = config.router.add_layout(content=RootLayout)
+root.add_page(path='/', content=intro_page)
+root.add_page(path='/components', content=components_page)
+
