a73ae8fba387827eff1fa83542cb984aa7926f09
Krzysztof Bielikowicz <krzysztof@causalens.com>
2025-09-24 17:30:43 +0100

    Export useRouterContext

--- packages/dara-core/js/router/index.tsx
+++ packages/dara-core/js/router/index.tsx
@@ -2,5 +2,5 @@ export { SingleUseCache as LockSecuredCache } from './cache';
 export { RouterContextProvider, useRouterContext } from './context';
 export { createRouter, findFirstPath } from './create-router';
 export { fetchRouteData, getFromPreloadCache, usePreloadRoute, type LoaderData } from './fetching';
-export { default as RouteContent, createRouteLoader, type LoaderResult } from './route-content';
+export { default as RouteContentDisplay, createRouteLoader, type LoaderResult } from './route-content';
 export { default as RouterRoot } from './router-root';
