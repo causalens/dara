9d7556fccce99956351a477d1af644637d492dcc
Krzysztof Bielikowicz <krzysztof@causalens.com>
2025-09-30 15:24:41 +0100

    Add unit test for navigate action

---
+++ packages/dara-core/tests/js/navigate-action.spec.tsx
@@ -0,0 +1,119 @@
+/* eslint-disable no-restricted-globals */
+import { act, renderHook } from '@testing-library/react';
+import * as React from 'react';
+import type { Mock } from 'vitest';
+
+import { NavigateTo } from '@/actions';
+import type { NavigateToImpl } from '@/types/core';
+
+function withMockHref(callback: (hrefSetter: Mock) => void, hrefValue?: string) {
+    const originalLocation = window.location;
+    try {
+        const mock = vi.fn();
+        Object.defineProperty(window, 'location', {
+            value: {
+                ...window.location,
+                get href() {
+                    return hrefValue ?? originalLocation.href;
+                },
+                set href(value) {
+                    mock(value);
+                },
+            },
+            writable: true,
+        });
+        callback(mock);
+    } finally {
+        window.location = originalLocation;
+    }
+}
+
+describe('NavigateTo action', () => {
+    beforeEach(() => {
+        window.dara = {
+            base_url: '',
+        } as any;
+    });
+
+    // string/object forms, relative links
+    it.each([
+        { url: './to' },
+        { url: '../to' },
+        { url: 'to' },
+        { url: { pathname: './to' } },
+        { url: { pathname: '../to' } },
+        { url: { pathname: 'to' } },
+    ])('should navigate relatively to "$url" using react router', ({ url }) => {
+        const ctx = {
+            navigate: vi.fn(),
+        };
+
+        NavigateTo(ctx as any, {
+            __typename: 'ActionImpl',
+            name: 'NavigateTo',
+            new_tab: false,
+            uid: 'uid',
+            url,
+        } satisfies NavigateToImpl);
+        // called with url, no options
+        expect(ctx.navigate).toHaveBeenCalledWith(url, undefined);
+    });
+
+    it.each([{ url: 'https://google.com' }, { url: 'http://example.com' }])(
+        'should navigate absolutely to "$url" using href',
+        ({ url }) => {
+            withMockHref((hrefSetter) => {
+                const ctx = {
+                    navigate: vi.fn(),
+                };
+
+                NavigateTo(ctx as any, {
+                    __typename: 'ActionImpl',
+                    name: 'NavigateTo',
+                    new_tab: false,
+                    uid: 'uid',
+                    url,
+                } satisfies NavigateToImpl);
+                // called with url
+                expect(hrefSetter).toHaveBeenCalledWith(url);
+            });
+        }
+    );
+
+    it('should navigate to same-origin absolute URLs using react router', () => {
+        const ctx = {
+            navigate: vi.fn(),
+        };
+
+        withMockHref(() => {
+            NavigateTo(ctx as any, {
+                __typename: 'ActionImpl',
+                name: 'NavigateTo',
+                new_tab: false,
+                uid: 'uid',
+                url: 'http://localhost:3000/another-page',
+            } satisfies NavigateToImpl);
+            // called with relative URL since it's same-origin
+            expect(ctx.navigate).toHaveBeenCalledWith('/another-page', undefined);
+        }, 'http://localhost:3000/test');
+    });
+
+    it('should navigate to same-origin absolute URLs with base-url using react router', () => {
+        window.dara.base_url = '/app';
+        const ctx = {
+            navigate: vi.fn(),
+        };
+
+        withMockHref(() => {
+            NavigateTo(ctx as any, {
+                __typename: 'ActionImpl',
+                name: 'NavigateTo',
+                new_tab: false,
+                uid: 'uid',
+                url: 'http://localhost:3000/app/another-page',
+            } satisfies NavigateToImpl);
+            // called with relative URL since it's same-origin
+            expect(ctx.navigate).toHaveBeenCalledWith('/another-page', undefined);
+        }, 'http://localhost:3000/app/test');
+    });
+});
