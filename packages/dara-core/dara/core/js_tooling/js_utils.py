3eb44c51b924961a134f5e969e3c4293568771cd
Krzysztof Bielikowicz <krzysztof@causalens.com>
2026-01-05 14:00:29 +0000

    Bump vite patch to 7.0.8

--- packages/dara-core/dara/core/js_tooling/js_utils.py
+++ packages/dara-core/dara/core/js_tooling/js_utils.py
@@ -369,7 +369,7 @@ class BuildCache(BaseModel):
         pkg_json['dependencies'] = {
             **deps,
             '@vitejs/plugin-react': '4.6.0',
-            'vite': '7.0.4',
+            'vite': '7.0.8',
             'vite-plugin-node-polyfills': '0.24.0',
         }
 
